1dfc3c7c96a0e7a646200bf123b254e106829aa1
moritzjuergens <moritzjuergens@outlook.de>
2022-07-09 18:18:04 +0200

    m

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -59,7 +59,7 @@
         </div>
       </div>
 
-      <div id="myModal" class="modal">
+      <div id="modal" class="modal">
         <!-- Modal content -->
         <div class="modal-content">
           <span class="close">&times;</span>
